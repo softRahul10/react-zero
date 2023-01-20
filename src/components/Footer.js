a94a0b3ca584cede051bad58aef3a664986fde1b
softRahul10 <softrahul10@gmail.com>
2023-01-20 23:16:11 +0530

    Footer is ready

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export function Footer() {
+    return (
+        <footer className='app-footer'>
+            <h4> Created By : Rahul Keshri </h4>
+        </footer>
+    );
+}
