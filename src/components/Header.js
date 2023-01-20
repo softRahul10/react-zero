d9778a5a7045f5a3955d91d2ef07da95e5dd635a
softRahul10 <softrahul10@gmail.com>
2023-01-20 22:35:38 +0530

    Header component ready

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+function Header() {
+    return (
+        <header className="app-header">
+            <div className="app-header-left">
+                <h3>Rahul Keshri</h3>
+            </div>
+            <div className="app-header-right">
+                <img src="" alt="" />
+            </div>
+        </header>
+    );
+}
+
+export default Header;
